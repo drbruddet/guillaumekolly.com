91db7a1c1ad29b134fb2faa9d2be669e892e04c2
Guillaume Kolly <guillaume.kolly@gmail.com>
2017-03-20 16:43:01 +0100

    creation of utils components

---
+++ components/utils/Title.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default ( {title} ) => <h1> {title} </h1>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 import HeadBloc from '../components/layout/HeadBloc'
+import Title from '../components/utils/Title'
+import Paragraph from '../components/utils/Paragraph'
 
 export default () => (
   <div>
     <HeadBloc />
+    <Title title="Guillaume Kolly" />
+    <Paragraph>
+      Text of the paragraph
+    </Paragraph>
   </div>
 )
