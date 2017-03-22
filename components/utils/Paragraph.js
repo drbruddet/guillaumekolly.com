709e9271133ffd9a65dc8f9ad8f89fd6807785e8
Guillaume Kolly <guillaume.kolly@gmail.com>
2017-03-22 14:42:02 +0100

    add react-typist and play the intro text

--- components/utils/Paragraph.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import css from 'next/css'
-
-export default ( {children} ) => <p className={p}> {children} </p>
-
-const p = css({
-  color: '#000',
-  fontSize: '1em',
-})
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import css from 'next/css'
+import Link from 'next/link'
+import Typist from 'react-typist'
 
 import HeadBloc from '../components/layout/HeadBloc'
 import Menu from '../components/Menu'
 import H1 from '../components/utils/H1'
-import Paragraph from '../components/utils/Paragraph'
 import socials from '../data/socials'
 
 export default class extends React.Component {
@@ -23,9 +24,17 @@ export default class extends React.Component {
         <section className={introduction}>
           <H1 title="Guillaume Kolly"
             style={{ color: '#000', fontSize: '4em' }} />
-          <Paragraph>
-            Text of the paragraph
-          </Paragraph>
+          <Typist className="Typist"
+            avgTypingSpeed={40}
+            startDelay={1000}
+            cursor={{ hideWhenDone: true }}>
+            I’m a full stack developer born and raised in the French Alps,
+            but I’m currently living in Lyon - France. I’m fluent in
+            Node / Meteor / JS / React, but I’m recently started
+            experimenting with GraphQL, Redux and Webpack. I also
+            have skills with Photoshop / Illustrator. Do not hezitate
+            to <Link href="#"><a>contact me</a></Link>.
+          </Typist>
         </section>
       </div>
     )
@@ -45,7 +54,13 @@ const introduction = css({
   position: 'absolute',
   right: '20%',
   top: '30%',
-  border: '1px solid blue',
+  width: '500px',
+  letterSpacing: '-0.6px',
+  '@media only screen and (min-width: 200px) and (max-width: 640px)': {
+    width: '80%',
+    right: '10px',
+    top: '5%',
+  }
 })
 
 const container = css({
