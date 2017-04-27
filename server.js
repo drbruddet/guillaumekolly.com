b7228b14c5d9ba6433145253b999651ab199b8a9
Guillaume Kolly <guillaume.kolly@gmail.com>
2017-04-27 15:08:03 +0200

    config server part 3

--- server.js
+++ server.js
@@ -24,8 +24,9 @@ app.prepare()
     return handle(req, res)
   })
 
-  server.listen(3000, (err) => {
+  server.listen(process.env.PORT || 3000, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
   })
-}).listen(process.env.PORT || 5000)
+
+})
